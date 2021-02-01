f49f5612f85817d41e1ba7ea1c6154aae100da23
Andrii <farewell483@gmail.com>
2021-02-01 11:32:04 +0200

    refactor

--- src/Service/YamlDriver.php
+++ src/Service/YamlDriver.php
@@ -3,10 +3,8 @@
 namespace App\Service;
 
 use App\Entity\Article;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\Yaml\Exception\ParseException;
 use Symfony\Component\Yaml\Yaml;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
  * Class YamlDriver
@@ -15,20 +13,6 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 class YamlDriver extends BaseDriver implements ExtensionDriverInterface
 {
     const YAML_MIME_TYPE = "application/x-yaml";
-
-    /** @var TranslatorInterface */
-    private $translator;
-
-    /**
-     * YamlDriver constructor.
-     * @param ValidatorInterface $validator
-     * @param TranslatorInterface $translator
-     */
-    public function __construct(ValidatorInterface $validator, TranslatorInterface $translator)
-    {
-        parent::__construct($validator);
-        $this->translator = $translator;
-    }
 
     /**
      * @param $content
@@ -39,7 +23,7 @@ class YamlDriver extends BaseDriver implements ExtensionDriverInterface
         try {
             $array = Yaml::parse($content);
         } catch (ParseException $e) {
-            $this->validErrors[] = $this->translator->trans("article.incorrect_yaml_structure");
+            $this->validErrors[] = "article.incorrect_yaml_structure";
             $array = [];
         }
 
